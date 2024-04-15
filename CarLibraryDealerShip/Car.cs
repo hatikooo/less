cfea405704d4dd88cf3847a4b2a22bb1a81d9a2c
hatikooo <hatikooo@tutamail.com>
2024-04-15 16:19:08 +0300

    fixed merge conflicts

---
+++ CarLibraryDealerShip/Car.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace CarLibraryDealerShip
+{
+    //Remember always to have an empty constructor in a Class that could be serialized
+    //If no constructor is defined, an empty constructor is default
+    public class Car
+    {
+        public string Model { get; set; }
+        public string Color { get; set; }
+        public string RegistrationNumber { get; set; }
+    }
+}
